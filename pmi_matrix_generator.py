fb7f1bda3dc4e1270606bd6141a4b8cc1f721b31
Robert Hopkins <rmhopkins4@gmail.com>
2023-07-30 20:37:02 -0400

    Add files via upload

--- pmi_matrix_generator.py
+++ pmi_matrix_generator.py
@@ -7,11 +7,11 @@ def reestimate_pmi_matrix(potential_cognates: list[tuple[str, str]], open_gap_sc
     print(f"number remaining: {num_remaining}")
 
     # pick out what will be used to reestimate
-    cognate_pairs_moving_on = []
-    aligned_cognate_pairs_moving_on = []
+    aligned_cognate_pairs_within_threshold = []
 
     # alignment cache for each loop
     alignment_cache = {}
+    alignment_cache.clear()
 
     # align cognates (get probable cognates) using past pmi matrix
     for cognate_pair in potential_cognates:
@@ -28,25 +28,27 @@ def reestimate_pmi_matrix(potential_cognates: list[tuple[str, str]], open_gap_sc
             pair_pmi_score, aligned_pair = alignment_cache[cache_key]
 
         if pair_pmi_score >= theta_pmi:
-            # this is shipped off to next iteration
-            cognate_pairs_moving_on.append(cognate_pair)
             # this is used to generate this PMI matrix
-            aligned_cognate_pairs_moving_on.append(aligned_pair)
+            aligned_cognate_pairs_within_threshold.append(aligned_pair)
 
     # the probable cognates are be used to re-estimate the pmi_matrix
     num_alignments = 0
     valid_alignments_count = Counter()
-    for aligned_pair in aligned_cognate_pairs_moving_on:
+    for aligned_pair in aligned_cognate_pairs_within_threshold:
         word1, word2 = aligned_pair
         for char1, char2 in zip(word1, word2):
-            num_alignments += 1
+
             if char1 == '-' or char2 == '-':
                 continue
 
-            char1, char2 = sorted([char1, char2])
-            valid_alignments_count[(char1, char2)] += 1
+            # just valid alignments
+            num_alignments += 1
+
+            sorted_c1, sorted_c2 = tuple(sorted([char1, char2]))
+            valid_alignments_count[(sorted_c1, sorted_c2)] += 1
 
     pmi_matrix = current_pmi_matrix
+    # update it
     for (alignment_char1, alignment_char2), alignment_count in valid_alignments_count.items():
         pmi_matrix[(alignment_char1, alignment_char2)] = math.log((alignment_count / num_alignments) /
                                                                   (
@@ -58,4 +60,4 @@ def reestimate_pmi_matrix(potential_cognates: list[tuple[str, str]], open_gap_sc
         return pmi_matrix
 
     else:
-        return reestimate_pmi_matrix(cognate_pairs_moving_on, open_gap_score, extend_gap_score, theta_pmi, char_counts, num_characters, pmi_matrix, num_remaining - 1)
+        return reestimate_pmi_matrix(potential_cognates, open_gap_score, extend_gap_score, theta_pmi, char_counts, num_characters, pmi_matrix, num_remaining - 1)
--- probably_related_generator.py
+++ probably_related_generator.py
@@ -1,11 +1,13 @@
 import multiprocessing
 from itertools import combinations
+import matplotlib.pyplot as plt
+
 
 from dERC_LDNcalculator import calculate_dERC_LDN
 from language import Language
 
 
-def __check_dERC_LDN_of_pairing(pairing: tuple[Language, Language], theta_dERC=0.70):
+def __check_dERC_LDN_of_pairing(pairing: tuple[Language, Language]):
     """
     Calculates the dERC/LDN of a pair of languages and returns the pair if the distance is below a provided threshold.
 
@@ -20,8 +22,7 @@ def __check_dERC_LDN_of_pairing(pairing: tuple[Language, Language], theta_dERC=0
     """
     L1, L2 = pairing
     dERC_of_pairing = calculate_dERC_LDN(L1, L2)
-    if dERC_of_pairing <= theta_dERC:
-        return pairing
+    return (pairing, dERC_of_pairing)
 
 
 def get_related_languages(training_set: dict[str, list[Language]], test_set: dict[str, list[Language]], theta_dERC=.70):
@@ -68,6 +69,45 @@ def get_related_languages(training_set: dict[str, list[Language]], test_set: dic
 
         # Collect the results of probably related languages in probably_related_languages
         probably_related_languages = [
-            pairing for pairing in results if pairing is not None]
+            pairing for pairing, score in results if score <= theta_dERC]
+
+        scores = [score for pairing, score in results]
+
+    # plot scoree
+    plot_scores(scores)
 
     return probably_related_languages
+
+
+def plot_scores(data_list: list[float]):
+    """
+    Plot the distribution of a list of floats.
+
+    Parameters:
+        data_list (list): A list of floats.
+
+    Returns:
+        None
+    """
+    # Check if data_list is not empty
+    if not data_list:
+        raise ValueError(
+            "The input list is empty. Please provide a non-empty list of floats.")
+
+    # Create the plot
+    plt.figure(figsize=(8, 6))
+
+    # Adjust the number of bins based on the size of the data
+    num_bins = min(len(data_list) // 10, 50)
+
+    # Create a histogram or density plot
+    plt.hist(data_list, bins=num_bins, density=True,
+             alpha=0.7, color='skyblue')
+
+    # Add labels and title
+    plt.xlabel("Values")
+    plt.ylabel("Frequency")
+    plt.title("Distribution Plot")
+
+    # Show the plot
+    plt.show()
